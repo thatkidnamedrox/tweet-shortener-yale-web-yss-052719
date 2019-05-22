3c2fc810e7dd40a0f03ae39c3b94b473c5112d83
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 21:00:52 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def dictionary
+  {
+    "hello" => 'hi',
+    "to, two, too" => '2',
+    "for, four" => '4',
+    'be' => 'b'.
+    'you' => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  dictionary = dictionary()
+  #tweet_array = tweet.split(" ")
+end
