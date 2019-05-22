422ee1bab19224004a8b860b6fdfca3aa00013f7
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 21:02:48 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,11 +1,11 @@
 # Write your code here.
 def dictionary
   {
-    "hello" => 'hi',
-    "to, two, too" => '2',
-    "for, four" => '4',
-    'be' => 'b'.
-    'you' => 'u',
+    "hello" => "hi",
+    "to, two, too" => "2",
+    "for, four" => "4",
+    "be" => "b",
+    "you" => "u",
     "at" => "@",
     "and" => "&"
   }
